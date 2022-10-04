2c6dea2e268455fa790a70b3dc3702d77ec5d3e3
Massimo-it <66207126+Massimo-it@users.noreply.github.com>
2022-10-04 15:43:55 +0200

    react router dom v6

--- progetto-myfreedom/src/navbar.js
+++ progetto-myfreedom/src/navbar.js
@@ -1,13 +1,14 @@
 import React, { useState } from "react";
 import {
+    BrowserRouter,
+    Routes,
     Route,
-    NavLink,
-    HashRouter
+    Link
   } from "react-router-dom";
+import Home from "./home";
 import Description from "./description";
 import Equipment from "./equipment";
 import Contact from "./contact";
-import Home from "./home";
 
 function Navbar() {
     const [navbarOpen, setNavbarOpen] = useState(false);
@@ -18,27 +19,31 @@ function Navbar() {
         setNavbarOpen(false);
       }
     return (
-        <HashRouter>
+        <BrowserRouter>
           <header className="headerC">
             <div className="headerContainer">
-              <NavLink to="/" onClick={() => closeMenu()} className="logo"><img className="logoImg" src={process.env.PUBLIC_URL + "/images/logo.png"} alt="logo"/> My Freedom</NavLink>
+              <Link to="/" onClick={() => closeMenu()} className="logo"><img className="logoImg" src={process.env.PUBLIC_URL + "/images/logo.png"} alt="logo"/> My Freedom</Link>
             </div>
           </header>
           <nav className="navBar">
               <button id="openClose" onClick={handleToggle}>{navbarOpen ? <img src={process.env.PUBLIC_URL + "/icons/close_black_24dp.svg"} alt="icona della croce"/> : <img src={process.env.PUBLIC_URL + "/icons/hamburger_24dp.svg"} alt="icona hamburger"/> } </button>
               <ul className={`menuNav ${navbarOpen ? " showMenu" : ""}`}>
-                <li><NavLink to="/description" className="position" onClick={() => closeMenu()}>Descrizione</NavLink></li>
-                <li><NavLink to="/equipment" className="position" onClick={() => closeMenu()}>Dotazioni</NavLink></li>
-                <li><NavLink to="/contact" className="position" onClick={() => closeMenu()}>Contatti</NavLink></li>
+                <li><Link to="description" className="position" onClick={() => closeMenu()}>Descrizione</Link></li>
+                <li><Link to="equipment" className="position" onClick={() => closeMenu()}>Dotazioni</Link></li>
+                <li><Link to="contact" className="position" onClick={() => closeMenu()}>Contatti</Link></li>
               </ul>
               <div>
-                <Route path="/description" component={Description} />
-                <Route path="/equipment" component={Equipment} />
-                <Route path="/contact" component={Contact} />
-                <Route exact path="/" component={Home} />
+
+                <Routes>
+                  <Route exact path="/" element={<Home />} />
+                  <Route path="description" element={<Description />} />
+                  <Route path="equipment" element={<Equipment />} />
+                  <Route path="contact" element={<Contact />} />
+                </Routes>
+
               </div>
           </nav>     
-        </HashRouter>
+        </BrowserRouter>
       );
 }
 export default Navbar;
